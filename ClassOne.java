bea2e38a647853c725e044b388355c84fafe3895
geeta gohil <gitgohil@yahoo.co.in>
2023-02-15 17:10:39 +0000

    ClassOne

---
+++ ClassOne.java
@@ -0,0 +1,9 @@
+package LerningOne;
+
+public class ClassOne {
+    public static void main(String[] args) {
+        System.out.println("technique");
+        System.out.println("Bva");
+        System.out.println("Ecp");
+    }
+}
